37f669e815e0a6e9264faf3a3d18664bc524166f
Roberto B. Costa <romeromatallica@gmail.com>
2024-02-06 12:20:01 -0300

    adicionando novos modelos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import  Model from '../public/models/earth/Earth';
+import Model from "../public/models/earth/Earth";
+import Dragon from "../public/models/dragon/Dragon";
 
 const Home = () => {
-
   return (
     <div>
-      <h1>Earth 3D</h1>
-
       <Canvas>
         <ambientLight intensity={1.1} /> // Adiciona uma luz ambiente.
         <OrbitControls /> // Adiciona controles de órbita para a câmera.
-        <Environment preset="night" /> // A predefinição pode ser alterada para qualquer outro efeito de iluminação.
-
+        <Environment preset="night" /> // A predefinição pode ser alterada para
+        qualquer outro efeito de iluminação.
         <mesh>
-          <Model />
+          {/* <Model /> */}
+          <Text>Silver Dragon Mir4</Text>
+          <Dragon />
         </mesh>
       </Canvas>
     </div>
